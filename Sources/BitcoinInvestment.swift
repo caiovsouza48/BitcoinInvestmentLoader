08cdea248eb5feb08839a7d64c3e6dddd6a536f7
caiovsouza48 <caio.vsouza48@gmail.com>
2025-03-16 10:06:17 -0300

    Created a public initializer for BitcoinInvestment to be able to instantiate on another modules

--- Sources/BitcoinInvestment.swift
+++ Sources/BitcoinInvestment.swift
@@ -12,4 +12,11 @@ public struct BitcoinInvestment: Equatable {
     public let bitcoinPriceAtPurchase: Decimal
     public let date: Date
     public let note: String?
+    
+    public init(amount: Decimal, bitcoinPriceAtPurchase: Decimal, date: Date, note: String?) {
+        self.amount = amount
+        self.bitcoinPriceAtPurchase = bitcoinPriceAtPurchase
+        self.date = date
+        self.note = note
+    }
 }
