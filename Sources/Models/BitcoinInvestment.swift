a436c8ecc20bd63f06cc4609575716f78e9cac70
caiovsouza48 <caio.vsouza48@gmail.com>
2025-05-31 21:05:21 -0300

    Included Money Framework to best represent Monetary values, since the user story now requires all currencies and Decimal are not enough to represent valid states of money alone

--- Sources/Models/BitcoinInvestment.swift
+++ Sources/Models/BitcoinInvestment.swift
@@ -6,16 +6,20 @@
 //
 
 import Foundation
+import Money
 
 public struct BitcoinInvestment: Identifiable, Equatable {
     
     public let id: UUID
-    public let amount: Decimal
-    public let bitcoinPriceAtPurchase: Decimal
+    public let amount: MonetaryValue
+    public let bitcoinPriceAtPurchase: MonetaryValue
     public let purchaseDate: Date
     public let note: String?
     
-    public init(id: UUID, amount: Decimal, bitcoinPriceAtPurchase: Decimal, purchaseDate: Date, note: String?) {
+    public init(id: UUID, amount: MonetaryValue, bitcoinPriceAtPurchase: MonetaryValue, purchaseDate: Date, note: String?) throws {
+        guard amount.currency.isEqual(to: bitcoinPriceAtPurchase.currency) else {
+            throw NSError(domain: "BitcoinInvestmentLoader", code: 1001, userInfo: [NSLocalizedDescriptionKey : "Currencies must be the same"])
+        }
         self.id = id
         self.amount = amount
         self.bitcoinPriceAtPurchase = bitcoinPriceAtPurchase
